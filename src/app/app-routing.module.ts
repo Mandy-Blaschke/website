d87da12fbf415704196624d9cc4102f68feef330
Mandy Blaschke <post@mandy-blaschke.de>
2020-11-21 10:57:07 +0100

    start working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {StartPageComponent} from './pages/start-page/start-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: 'start'},
+  {path: 'start', component: StartPageComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StartPageComponent } from './pages/start-page/start-page.component';
+import { HeaderComponent } from './common/header/header.component';
+import { FooterComponent } from './common/footer/footer.component';
+import { PortfolioPageComponent } from './pages/portfolio-page/portfolio-page.component';
+import { QualificationPageComponent } from './pages/qualification-page/qualification-page.component';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
+import { ButtonComponent } from './common/button/button.component';
+import { WelcomePageComponent } from './pages/welcome-page/welcome-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StartPageComponent,
+    HeaderComponent,
+    FooterComponent,
+    PortfolioPageComponent,
+    QualificationPageComponent,
+    ContactPageComponent,
+    ButtonComponent,
+    WelcomePageComponent
   ],
   imports: [
     BrowserModule,
